2b4f8e6186ce5526566f659968e3e970c1232005
irizaharii-tech <iri.zaharii@gmail.com>
2025-10-16 12:46:05 +0200

    Delete client/src/SubcontractorPage.jsx

--- client/src/SubcontractorPage.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default function Subcontractor(){ return (<div><h1>Subcontractor Page</h1><p>Use dedicated subcontractor UI in production. Use API endpoints to view assigned jobs and update status.</p></div>) }
