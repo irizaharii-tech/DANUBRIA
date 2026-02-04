b18bf1c48613e4443cfcd97302e7f7778333636b
irizaharii-tech <iri.zaharii@gmail.com>
2026-02-04 16:16:49 +0100

    Delete client directory

--- client/src/pages/Landing.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default function Landing(){ return (<div style={{textAlign:'center'}}><img src='/logo.png' alt='logo' style={{maxWidth:520, width:'60%'}}/><h2>The flow of Europe, the power of logistics.</h2><a href='/login' style={{background:'#0b74d1', color:'#fff', padding:10, borderRadius:6, textDecoration:'none'}}>Login to Client Portal</a></div>) }
